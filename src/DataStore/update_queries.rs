0de4a7f91a8054d145d7a366b1b8d3d96fc31cd8
RonaldColyar <phantexcreations@gmail.com>
2021-12-12 21:04:34 -0600

    wip more update queries

--- src/DataStore/update_queries.rs
+++ src/DataStore/update_queries.rs
@@ -30,6 +30,48 @@ SET = numAttending = $1,
 WHERE Id = $3;   
 ";
 
+pub const UPDATE_GITHUB_ACCESS_TOKEN_QUERY: &str = "
+UPDATE user 
+SET githubAccessToken = $1
+WHERE Id = $2;
+";
+
+pub const UPDATE_DISCORD_ACCESS_TOKEN_QUERY: &str = "
+UPDATE user 
+SET discordAccessToken = $1
+WHERE Id = $2;
+";
+
+pub const UPDATE_CONTRIBUTIONS_QUERY: &str = "
+UPDATE user
+SET contributions = $1
+WHERE Id = $2;
+";
+
+pub const UPDATE_BANNER_URL_QUERY: &str = "
+UPDATE user
+SET bannerUrl = $1
+WHERE Id = $2;
+";
+
+pub const UPDATE_BIO_QUERY: &str = "
+UPDATE user
+SET bio = $1
+WHERE Id = $2;
+";
+
+pub const UPDATE_LAST_ONLINE_QUERY: &str = "
+UPDATE user
+SET lastOnline = $1
+WHERE Id = $2;
+";
+
+pub const UPDATE_USER_NAME_QUERY: &str = "
+UPDATE user
+SET userName = $1
+WHERE Id = $2;
+";
+
 pub const BAN_USER_QUERY: &str = "
 UPDATE user
 SET banned = $1,
