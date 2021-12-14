77d98768ab4434afdddd0bdfc464ab64ebe91772
RonaldColyar <phantexcreations@gmail.com>
2021-12-13 20:51:16 -0600

    tests passed

--- src/DataStore/test.rs
+++ src/DataStore/test.rs
@@ -6,18 +6,27 @@ use tokio_postgres::{Client,row::Row,NoTls,Error};
 pub async fn test(){
     let mut execution_handler = setup_execution_handler().await.unwrap();
     setup_tables(&mut execution_handler).await;
-    let user_id = tests::user::test_insert_and_gather_user(&mut execution_handler).await;   
-    tests::user::test_updating_user_avatar(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_updating_ban_status_of_user(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_updating_user_display_name(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_updating_user_bio(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_updating_last_online(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_updating_github_access_token(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_updating_discord_access_token(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_update_contributions(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_update_banner_url(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_update_user_name(&mut execution_handler, user_id.clone()).await;
-    tests::user::test_updating_entire_user(&mut execution_handler).await;
+    test_users(&mut execution_handler).await;
+    test_room().await;
+}
+
+async fn test_room(){
+
+}
+
+async fn test_users(execution_handler: &mut ExecutionHandler){
+    let user_id = tests::user::test_insert_and_gather_user(execution_handler).await;   
+    tests::user::test_updating_user_avatar(execution_handler, user_id.clone()).await;
+    tests::user::test_updating_ban_status_of_user(execution_handler, user_id.clone()).await;
+    tests::user::test_updating_user_display_name(execution_handler, user_id.clone()).await;
+    tests::user::test_updating_user_bio(execution_handler, user_id.clone()).await;
+    tests::user::test_updating_last_online(execution_handler, user_id.clone()).await;
+    tests::user::test_updating_github_access_token(execution_handler, user_id.clone()).await;
+    tests::user::test_updating_discord_access_token(execution_handler, user_id.clone()).await;
+    tests::user::test_update_contributions(execution_handler, user_id.clone()).await;
+    tests::user::test_update_banner_url(execution_handler, user_id.clone()).await;
+    tests::user::test_update_user_name(execution_handler, user_id.clone()).await;
+    tests::user::test_updating_entire_user(execution_handler).await;
 }
 
 async fn setup_tables(execution_handler:&mut ExecutionHandler){
