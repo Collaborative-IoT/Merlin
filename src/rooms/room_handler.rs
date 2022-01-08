232cfaeeb76cfab06398bfb755946b47507e53eb
RonaldColyar <phantexcreations@gmail.com>
2022-01-08 12:07:28 -0600

    fanning

---
+++ src/rooms/room_handler.rs
@@ -0,0 +1,14 @@
+use crate::state::state::ServerState;
+use futures::lock::Mutex;
+use std::sync::Arc;
+use crate::communication::data_capturer;
+use crate::data_store::sql_execution_handler::ExecutionHandler;
+
+pub async fn remove_user_from_user(
+    user_id:i32, 
+    requester_id:i32, 
+    server_state:&Arc<Mutex<ServerState>>,
+    execution_handler:&Arc<Mutex<ServerState>>
+    ){
+   
+}
