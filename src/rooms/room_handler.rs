cf5787e27d8e351c463c91c4f3cb6981d40ca778
RonaldColyar <phantexcreations@gmail.com>
2022-01-09 05:17:06 -0600

    commentation

--- src/rooms/room_handler.rs
+++ src/rooms/room_handler.rs
@@ -5,7 +5,21 @@ use crate::communication::data_capturer;
 use crate::data_store::sql_execution_handler::ExecutionHandler;
 use crate::communication::communication_types::{UserRemovedFromRoom};
 
-pub async fn remove_user_from_user(
+
+//managing rooms happens in a pub-sub fashion
+//the client waits on the response from this server
+//and this server waits on the response from the
+//voice server via rabbitMQ(spawned in another task)
+//once this server gathers a response, it fans it
+//to all involved parties(usually everyone in he room)
+//For Example, kicking someone:
+//1. admin requests user x to be kicked
+//2. this server sends the request to the voice server
+//3. once the voice server responds, if it is success
+//the user is removes from the state of the server
+//and this update is fanned/brodcasted across all users in the room.
+
+pub async fn remove_user_from_room(
     user_id:i32, 
     requester_id:i32, 
     server_state:&Arc<Mutex<ServerState>>,
