6ee8b66c9dc60b45b220670afb476f56b845c079
RonaldColyar <phantexcreations@gmail.com>
2022-01-13 20:29:13 -0600

    type formatting

--- src/rabbitmq/rabbit.rs
+++ src/rabbitmq/rabbit.rs
@@ -12,4 +12,37 @@ pub async fn setup_rabbit_connection()->Result<Connection>{
     return Ok(conn);
 }
 
+pub async fn setup_consume_task(conn:&Connection)->Result<()>{
+    let channel = conn.create_channel().await?;
+    //declare/create new main queue
+    channel.queue_declare(
+        "main",
+        QueueDeclareOptions::default(),
+        FieldTable::default(),
+    ).await?;
+
+    let mut consumer = channel
+        .basic_consume(
+            "main",
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+    //listen for messages forever
+    tokio::task::spawn(async move{
+        while let Some(delivery) = consumer.next().await {
+            let (_, delivery) = delivery.expect("error in consumer");
+            delivery
+                .ack(BasicAckOptions::default())
+                .await
+                .expect("ack");
+        }
+    });
+
+
+    return Ok(());
+}
+
 pub async fn publish_message_to_voice_server(message: String) {}
