1c0e01745f3167f88837bd99dc0610c89fd88e07
RonaldColyar <phantexcreations@gmail.com>
2022-01-13 19:25:07 -0600

    working on rabbit init

--- src/rabbitmq/rabbit.rs
+++ src/rabbitmq/rabbit.rs
@@ -1,5 +1,6 @@
 
 use futures_util::stream::StreamExt;
+use tokio_amqp::*;
 use lapin::{
     options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Connection,
     ConnectionProperties, Result,
@@ -7,8 +8,8 @@ use lapin::{
 
 pub async fn setup_rabbit_connection()->Result<Connection>{
     let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
-    let conn = Connection::connect(&addr, ConnectionProperties::default().with_tokio()).await?; // Note the `with_tokio()` here
-    let channel:Connection = conn.create_channel().await?;
+    let conn:Connection = Connection::connect(&addr, ConnectionProperties::default().with_tokio()).await?; // Note the `with_tokio()` here
+    return Ok(conn);
 }
 
 pub async fn publish_message_to_voice_server(message: String) {}
