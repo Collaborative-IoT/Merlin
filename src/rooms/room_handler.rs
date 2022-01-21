45a8980529d82949e08bd59902ac39c229d0e55a
RonaldColyar <phantexcreations@gmail.com>
2022-01-20 21:09:20 -0600

    destroy room implementation

--- src/rooms/room_handler.rs
+++ src/rooms/room_handler.rs
@@ -1,17 +1,18 @@
 use crate::communication::communication_types::{
-    BasicResponse, RoomPermissions, VoiceServerClosePeer,
-    VoiceServerJoinAsSpeaker,VoiceServerCreateRoom
+    BasicResponse, RoomPermissions, VoiceServerClosePeer, VoiceServerCreateRoom,
+    VoiceServerDestroyRoom, VoiceServerJoinAsSpeaker,
 };
-use lapin::Channel;
 use crate::communication::data_capturer::CaptureResult;
 use crate::communication::{data_capturer, data_fetcher};
-use crate::data_store::db_models::{DBRoomBlock,DBRoom};
+use crate::data_store::db_models::{DBRoom, DBRoomBlock};
 use crate::data_store::sql_execution_handler::ExecutionHandler;
 use crate::rabbitmq::rabbit;
 use crate::state::state::ServerState;
 use crate::state::state_types::Room;
 use futures::lock::Mutex;
-use std::collections::{HashMap,HashSet};
+use lapin::Channel;
+use std::collections::{HashMap, HashSet};
+use std::mem::drop;
 use std::sync::Arc;
 use warp::ws::Message;
 
@@ -37,7 +38,6 @@ pub async fn block_user_from_room(
     publish_channel: &Arc<Mutex<lapin::Channel>>,
 ) {
     let mut state = server_state.lock().await;
-    let mut handler = execution_handler.lock().await;
 
     //this room exist and the user is inside of the room
     if state.rooms.contains_key(&room_id)
@@ -48,6 +48,7 @@ pub async fn block_user_from_room(
             .user_ids
             .contains(&user_id)
     {
+        let mut handler = execution_handler.lock().await;
         let owner_gather: (bool, i32, String) =
             data_fetcher::get_room_owner_and_settings(&mut handler, &room_id).await;
 
@@ -62,6 +63,7 @@ pub async fn block_user_from_room(
             };
             let capture_result =
                 data_capturer::capture_new_room_block(&mut handler, &new_block).await;
+            drop(handler); //don't hold guard longer than needed
             handle_user_block_capture_result(
                 capture_result,
                 requester_id,
@@ -82,44 +84,54 @@ pub async fn block_user_from_room(
     );
 }
 
-pub async fn create_room(      
+pub async fn create_room(
     server_state: &mut ServerState,
     publish_channel: &Arc<Mutex<lapin::Channel>>,
     execution_handler: &Arc<Mutex<ExecutionHandler>>,
     requester_id: i32,
-    public:bool){
-
-        let mut handler = execution_handler.lock().await;
+    public: bool,
+) {
+    let mut handler = execution_handler.lock().await;
+    let db_room = DBRoom {
+        id: -1,
+        owner_id: requester_id,
+        chat_mode: "fast".to_owned(),
+    };
+    let room_id = data_capturer::capture_new_room(&mut handler, &db_room).await;
+    if room_id == -1 {
+        send_error_to_requester_channel(
+            "internal error".to_string(),
+            requester_id,
+            server_state,
+            "issue_creating_room".to_string(),
+        );
+    } else {
         let channel = publish_channel.lock().await;
-        let db_room = DBRoom{
-            id: -1,
-            owner_id: requester_id,
-            chat_mode: "fast".to_owned(),
-        };
-        let room_id = data_capturer::capture_new_room(&mut handler, &db_room).await;
-        if room_id == -1{
-            send_error_to_requester_channel(
-                "internal error".to_string(),
-                requester_id,
-                server_state,
-                "issue_creating_room".to_string(),
-            );
-        }
-        else{
-            continue_with_successful_room_creation(room_id, &channel, public, server_state).await;
-        }
+        continue_with_successful_room_creation(room_id, &channel, public, server_state).await;
+    }
 }
 
+pub async fn destroy_room(
+    server_state: &mut ServerState,
+    publish_channel: &Arc<Mutex<lapin::Channel>>,
+    execution_handler: &Arc<Mutex<ExecutionHandler>>,
+    room_id: &i32,
+) {
+    //remove from db
+    let mut handler = execution_handler.lock().await;
+    data_capturer::capture_room_removal(&mut handler, room_id).await;
+    drop(handler);
 
-//executed after database insertion is proven to be successful
-async fn continue_with_successful_room_creation(room_id:i32, channel: &Channel, public:bool,server_state: &mut ServerState,){
-    let request_to_voice_server = VoiceServerCreateRoom {
-        roomId:room_id.clone().to_string()
+    //remove from state
+    server_state.rooms.remove(room_id);
+
+    //remove from voice server
+    let request_to_voice_server = VoiceServerDestroyRoom {
+        roomId: room_id.to_string(),
     };
     let request_str: String = serde_json::to_string(&request_to_voice_server).unwrap();
-    rabbit::publish_message(channel, request_str).await;
-    let new_room_state:Room = construct_basic_room_for_state(room_id.clone(), public);
-    server_state.rooms.insert(room_id, new_room_state);
+    let channel = publish_channel.lock().await;
+    rabbit::publish_message(&channel, request_str).await;
 }
 
 pub async fn remove_user_from_room_basic(
@@ -150,11 +162,7 @@ pub async fn join_room_as_speaker(
     let user_id: i32 = request_to_voice_server.peerId.parse().unwrap();
     //could be more efficient by only selecting single row
     let all_room_permissions: (bool, HashMap<i32, RoomPermissions>) =
-        data_fetcher::get_room_permissions_for_users(
-            &room_id,
-            &mut handler,
-        )
-        .await;
+        data_fetcher::get_room_permissions_for_users(&room_id, &mut handler).await;
     //if no errors gathering data
     if all_room_permissions.0 == false {
         let current_user_permissions: &RoomPermissions =
@@ -226,14 +234,30 @@ fn add_user_to_room_state(room_id: &i32, user_id: i32, state: &mut ServerState) 
         .insert(user_id);
 }
 
-fn construct_basic_room_for_state(room_id:i32, public:bool)->Room{
-    return Room{
-            room_id:room_id,
-            muted:HashSet::new(),
-            voice_server_id:0.to_string(),//not yet implemented(feature)
-            deaf:HashSet::new(),
-            user_ids:HashSet::new(),
-            public:public,
-            auto_speaker:false
+fn construct_basic_room_for_state(room_id: i32, public: bool) -> Room {
+    return Room {
+        room_id: room_id,
+        muted: HashSet::new(),
+        voice_server_id: 0.to_string(), //not yet implemented(feature)
+        deaf: HashSet::new(),
+        user_ids: HashSet::new(),
+        public: public,
+        auto_speaker: false,
     };
+}
+
+//executed after database insertion is proven to be successful
+async fn continue_with_successful_room_creation(
+    room_id: i32,
+    channel: &Channel,
+    public: bool,
+    server_state: &mut ServerState,
+) {
+    let request_to_voice_server = VoiceServerCreateRoom {
+        roomId: room_id.clone().to_string(),
+    };
+    let request_str: String = serde_json::to_string(&request_to_voice_server).unwrap();
+    rabbit::publish_message(channel, request_str).await;
+    let new_room_state: Room = construct_basic_room_for_state(room_id.clone(), public);
+    server_state.rooms.insert(room_id, new_room_state);
 }
