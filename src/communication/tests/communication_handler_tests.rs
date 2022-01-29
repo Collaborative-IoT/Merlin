1a9f668d20e90fe4f2c9ceabfa19c6eddd523ac4
RonaldColyar <phantexcreations@gmail.com>
2022-01-28 22:37:57 -0600

    minor commenting

---
+++ src/communication/tests/communication_handler_tests.rs
@@ -0,0 +1,19 @@
+/*
+The communication handler tests aims at
+testing the logic that the handler enforces.
+
+It doesn't test its consuming modules like the 
+execution handler, it makes sure
+requests fail under certain circumstances,
+state is being modified and messages are being
+persisted to the voice server via RabbitMq.
+
+This test isn't integration based, so we manually
+grab the messages intended for the voice server after
+publish.
+
+*/
+
+pub async fn test(){
+
+}
